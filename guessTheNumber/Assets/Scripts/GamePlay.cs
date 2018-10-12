54cca1fe7aa2e9b50f28318f19ae02aee7568f56
iam9 <ibrahim.asif.mirza@gmail.com>
2018-10-12 16:55:12 +0200

    A simple 'guess the number' game.

---
+++ guessTheNumber/Assets/Scripts/GamePlay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GamePlay : MonoBehaviour 
+{
+	private int numberSelected;
+	public Text inputField;
+	public Text output;
+
+	// Use this for initialization
+	void Start () 
+	{
+		numberSelected = Randomizer();
+		Debug.Log("Number selected: " + numberSelected);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyUp(KeyCode.Return) || (Input.GetKeyUp(KeyCode.KeypadEnter)))
+		{
+			if (inputField.text == numberSelected.ToString())
+				output.text = "Correct!";
+			if (int.Parse(inputField.text) > numberSelected)
+				output.text = "No... the number is lower than that.";
+			if (int.Parse(inputField.text) < numberSelected)
+				output.text = "No... it's a bigger number.";
+		}
+	}
+
+	private int Randomizer()
+	{
+		return Random.Range(1,99);
+	}
+}
