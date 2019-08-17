b4a97d4a1acc0b9e5a1510fb252ff48a23ea7b6a
Ibrahim <->
2019-08-17 19:57:12 +0200

    feat(NumberWizard)

---
+++ number_wizard/Assets/NumberWizard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Hello and welcome to Number Wizard");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+		
+	}
+}
