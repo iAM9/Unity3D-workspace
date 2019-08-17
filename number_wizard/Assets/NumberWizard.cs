c54075db73f64f6b1e991bacae428ae9c5f51ce4
Ibrahim <->
2019-08-17 20:03:57 +0200

    feat(new)

--- number_wizard/Assets/NumberWizard.cs
+++ number_wizard/Assets/NumberWizard.cs
@@ -14,13 +14,31 @@ public class NumberWizard : MonoBehaviour {
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
+
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = higher, Push Down = Lower, Push Enter = Correct");
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("You pressed UP!!!");
+        }
 
-        
-		
-	}
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("You pressed Down!!!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You pressed ENTER!!!");
+        }
+
+
+
+    }
 }
